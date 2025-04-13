410bc3f99388052b4e11582c9d9486e75df2ed3e
massirr <124568542+massirr@users.noreply.github.com>
2025-04-13 17:15:48 +0200

    tailwind_init

---
+++ src/pages/homePage.vue
@@ -0,0 +1,8 @@
+<script>
+
+</script>
+
+<!--for tailwind@3 to work, you need tailwind.css in assets, postcss.config.js and tailwind.config.js -> make sure you know the version you are working with-->
+<template>
+    <h1 class="text-2xl bg-red-500">This is my home page</h1>
+</template>
